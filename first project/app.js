595209a97c8221a2a4ed230a69ccb814e5848de0
Harshit garg2611 <hgarg4145@gmail.com>
2024-04-17 02:00:22 +0530

    .

--- first project/app.js
+++ first project/app.js
@@ -1,3 +1,4 @@
-let firstName = prompt("Enter first name:");
-let lastName = prompt("Last name :");
-alert(`Welcome ${firstName} ${lastName}`);
+// let firstName = prompt("Enter first name:");
+// let lastName = prompt("Last name :");
+// alert(`Welcome ${firstName} ${lastName}`);
+let msg = "   hello ";
