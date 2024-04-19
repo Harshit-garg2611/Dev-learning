5dc76c7236ad150ce046065c8c38700b938b97e1
Harshit garg2611 <hgarg4145@gmail.com>
2024-04-19 16:11:20 +0530

    C:/Program Files/Git/

---
+++ dice.js
@@ -0,0 +1,5 @@
+function roll(){
+    confirm("Want to roll a dice");
+     console.log(Math.floor(Math.random()*6) + 1);
+}
+roll();
--- random.js
+++ random.js
@@ -1,22 +1,31 @@
 // const min = prompt("Enter min. no");
-const max = prompt("Enter max. no: ");
-console.log("write quit to end ! ");
-const output = Math.floor(Math.random() * (max))+1;
-let guess = (prompt("guess the number"));
-while (true) {
-    if(guess=="quit"){
-        break;
-    }
-    if (guess == output) {
-        confirm("You guess the right no.");
-        break;
-    } else {
-        if(guess<output){
-           guess = prompt("enter large");
-        }else{
-            guess = prompt("enter small");
+// const max = prompt("Enter max. no: ");
+// console.log("write quit to end ! ");
+// const output = Math.floor(Math.random() * (max))+1;
+// let guess = (prompt("guess the number"));
+// while (true) {
+//     if(guess=="quit"){
+//         break;
+//     }
+//     if (guess == output) {
+//         confirm("You guess the right no.");
+//         break;
+//     } else {
+//         if(guess<output){
+//            guess = prompt("enter large");
+//         }else{
+//             guess = prompt("enter small");
             
-        }
-    }
+//         }
+//     }
     
-}
+// }
+// function outer(){
+//     let x = 4;
+//     let y = 5;
+//     function inner(){
+//         console.log(x);
+//         console.log(y);
+//     }
+//     inner();
+// }
