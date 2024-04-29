e88001cb1b3b3ba84ad49e3e43895977885e0718
Harshit garg2611 <hgarg4145@gmail.com>
2024-04-30 02:57:43 +0530

    merge sort

---
+++ localCode/sorting/mergeSorting.java
@@ -0,0 +1,36 @@
+
+    public class mergeSorting {
+        public static void merge(int[] arr, int si, int mid, int ei){
+            int temp[] = new int[ei-si+1];
+            int i = si;
+            int j=mid+1;
+            int k=0;
+            while(i<=mid&&j<=ei){
+                if(arr[i]<arr[j]){
+                    temp[k++]=arr[i++];
+                }else{
+                    temp[k++]=arr[j++];
+                }
+            }
+            while(i<=mid){
+                temp[k++]=arr[i++];
+            }
+            while(j<=ei){
+                 temp[k++]=arr[j++];
+            }
+            for(k=0,i=si;k<temp.length;k++,i++){
+                arr[i]=temp[k];
+            }
+        }
+        public static void mergeSort(int[] arr, int l, int r){
+            // Write your code here
+            if(l>=r){
+                return;
+            }
+            int mid = l + (r-l)/2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid+1, r);
+            merge(arr, l, mid, r);
+        }
+    }
+    
