d39313a05266f712826f0d3e4be395f01eb0b541
Harshit garg2611 <hgarg4145@gmail.com>
2024-05-29 22:54:37 +0530

    ejs

---
+++ backend/EJSdir/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port  = 8080;
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "/views"))
+app.get("/html", (req, res)=>{
+    console.log("request received");
+    res.render("home.ejs")
+})
+
+app.get("/", (req,res)=>{
+    res.send("home");
+})
+
+app.get("/rolldice", (req, res)=>{
+    let diceVal = Math.floor(Math.random() * 6 )+1 ; 
+    res.render("rollDice", {num:diceVal})
+})
+app.listen(port, ()=>{
+    console.log(`listening.. on ${port}`);
+})
