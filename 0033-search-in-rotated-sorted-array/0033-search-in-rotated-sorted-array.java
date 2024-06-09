e9b1dc6c8a6737095879369036ff7c5256ea0b2e
Harshit Garg <155181467+Harshit-garg2611@users.noreply.github.com>
2024-06-09 16:46:41 +0530

    Time: 0 ms (100%), Space: 42 MB (31.12%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,23 +3,23 @@ class Solution {
         int start = 0;
         int end = nums.length-1;
         while(start<=end){
-            int mid = start + (end - start)/2;
+            int mid = start + (end-start)/2;
             if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[start]<=nums[mid]){
-                if(nums[start]<=target && nums[mid]>target){
-                    end = mid-1;
-            }    else{
+                return mid ;
+            } 
+            if(nums[mid]>=nums[start]){
+                if(nums[start]<=target && target<nums[mid]){
+                end = mid-1;
+                  }else{
                 start = mid+1;
                   }
-            }else{
-                if(nums[mid]<target && nums[end]>=target){
+             }else{
+                if(nums[end]>=target && target>nums[mid]){
                     start = mid+1;
                 }else{
                     end = mid-1;
                 }
-            }
+             }
         }
         return -1;
     }
