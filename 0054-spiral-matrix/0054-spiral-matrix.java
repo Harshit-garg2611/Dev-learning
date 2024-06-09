eab4fe8222881894a241315a1b8693a9ea58ec13
Harshit Garg <155181467+Harshit-garg2611@users.noreply.github.com>
2024-06-09 16:17:40 +0530

    Time: 0 ms (100%), Space: 41.3 MB (49.92%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -5,31 +5,36 @@ class Solution {
         int endRow = matrix.length-1;
         int endCol = matrix[0].length-1;
         List<Integer> ans = new ArrayList<>();
-        while(startRow<=endRow && startCol<=endCol ){
-            // top row 
-            for(int j=startCol;j<=endCol;j++){
+        while(startRow<=endRow && startCol<=endCol){
+            // upper row
+            for(int j=startCol ; j<=endCol ; j++){
                 ans.add(matrix[startRow][j]);
             }
+
             // right col
             for(int i=startRow+1;i<=endRow;i++){
                 ans.add(matrix[i][endCol]);
             }
-            // bottom row 
+
+            // lower row
             for(int j=endCol-1;j>=startCol;j--){
                 if(startRow==endRow){
                     break;
                 }
                 ans.add(matrix[endRow][j]);
             }
-            // left col
+
+            // lefft col
             for(int i=endRow-1;i>startRow;i--){
                 if(startCol==endCol){
                     break;
                 }
                 ans.add(matrix[i][startCol]);
             }
-            // update 
-            startRow++; startCol++; endRow--; endCol--;
+            startRow++;
+            startCol++;
+            endRow--; endCol--;
+
         }
         return ans;
     }
